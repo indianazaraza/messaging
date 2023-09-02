70664310b6cf529df445263a4bac741c3b349fd9
Macarena Acosta <macarenaa988@gmail.com>
2023-09-02 16:30:42 -0300

    added classes to find locators

---
+++ src/main/java/com/android/mms/ui/Chat.java
@@ -0,0 +1,10 @@
+package com.android.mms.ui;
+
+public class Chat {
+    public static String contactNameInput = "com.android.mms:id/recipients_editor";
+    public static String sendBtn = "com.android.mms:id/send_button_sms";
+    public static String msgInput = "com.android.mms:id/embedded_text_editor";
+    public static String deliveryIcon = "com.android.mms:id/delivered_indicator";
+    public static String msgDeliveredView = "com.android.mms:id/text_view";
+    public static String backArrowBtn = "Navigate up";
+}
---
+++ src/main/java/com/android/mms/ui/Home.java
@@ -0,0 +1,7 @@
+package com.android.mms.ui;
+
+public class Home {
+    public static String newMsgBtn = "com.android.mms:id/action_compose_new";
+    public static String moreOptionsBtn = "android.widget.ImageButton"; //"//android.widget.ImageButton[@content-desc=\"More options\"]";
+    public static String deleteAllMsgOption = "android:id/title";
+}
